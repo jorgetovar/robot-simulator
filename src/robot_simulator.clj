5deab9f2d986ce86bf7bfe401025f2c4c09ddcb5
jorgetovar <jorgetovar621@gmail.com>
2021-01-07 19:32:35 -0500

    Refactor with reduce

--- src/robot_simulator.clj
+++ src/robot_simulator.clj
@@ -4,41 +4,31 @@
 (defn robot [coordinates, bearing]
       {:bearing bearing :coordinates coordinates})
 
-(def directions {:west :north
-                 :north :east
-                 :east :south
-                 :south :west})
+(def directions {:west :north :north :east :east :south :south :west})
 
-(defn turn-right [bearing]
-      (bearing directions))
+(defn turn-right [bearing] (bearing directions))
 
-(defn turn-left [bearing]
-      (bearing (set/map-invert directions)))
+(defn turn-left [bearing] (bearing (set/map-invert directions)))
+
+(defn advance [rbt]
+      (let [{:keys [coordinates bearing]} rbt]
+           (case bearing
+                 :east (update coordinates :x inc)
+                 :west (update coordinates :x dec)
+                 :north (update coordinates :y inc)
+                 :south (update coordinates :y dec)
+                 )))
 
 (defn robot-left [rbt] (assoc rbt :bearing (turn-left (:bearing rbt))))
 
 (defn robot-right [rbt] (assoc rbt :bearing (turn-right (:bearing rbt))))
 
-(defn robot-advance [rbt]
-      (assoc rbt :coordinates (let [{:keys [coordinates bearing]} rbt]
-                                   (case bearing
-                                         :east (update coordinates :x inc)
-                                         :west (update coordinates :x dec)
-                                         :north (update coordinates :y inc)
-                                         :south (update coordinates :y dec)
-                                         )
-                                   ))
+(defn robot-advance [rbt] (assoc rbt :coordinates (advance rbt)))
 
-      )
+(defn letter->action [letter rbt]
+      (case (str letter)
+            "L" (robot-left rbt)
+            "A" (robot-advance rbt)
+            "R" (robot-right rbt)))
 
-(defn letter->action [letter]
-      (case letter
-            "L" robot-left
-            "A" robot-advance
-            "R" robot-right))
-
-(defn inst->actions [inst]
-      (map letter->action (map str (seq inst))))
-
-(defn simulate [inst rbt]
-      ((apply comp (inst->actions (reverse inst))) rbt))
+(defn simulate [instructions rbt] (reduce #(letter->action %2 %1) rbt instructions))
